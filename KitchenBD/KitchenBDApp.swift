3f4ebeef6b4d5e6d9d1f7c81dedf06890bb17561
Mahbub Hasan <admin@imahbub.com>
2025-07-26 23:27:30 +0600

    Initial Commit

---
+++ KitchenBD/KitchenBDApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenBDApp.swift
+//  KitchenBD
+//
+//  Created by Md. Mahbub Hasan on 26/7/25.
+//
+
+import SwiftUI
+
+@main
+struct KitchenBDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
